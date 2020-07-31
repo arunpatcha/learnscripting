a0360326a9464f4e04a8586956fb34bf5f7a40c3
arun <arunpatcha@>
2020-07-31 22:29:43 +0530

    added comments in helloworld.sh

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # learnscripting
 learning examples for shell scripting
 
+1. Creating script printing hello world
+2. Added executble permission to the helloworld.sh
+
--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Purpose:  starting script
+# Date : Fri Jul 31 22:25:22 IST 2020
+# modification: Fri Jul 31 22:25:22 IST 2020
 echo ""Hello World""
+
+echo " Horray you executed first script"
