16546ea4ef89ea8906b893d581ba4266f67bfe49
arun <arunpatcha@>
2020-07-31 23:03:49 +0530

    Program 3 : basic admin tasks

--- scripts/basicadmintask.sh
+++ scripts/basicadmintask.sh
@@ -4,3 +4,17 @@
 # Date : Fri Jul 31 22:25:22 IST 2020
 # modification: Fri Jul 31 22:25:22 IST 2020
 # Program 3
+
+
+echo This script will run basic admin commands
+echo "$SHELL"
+echo
+top | head -20
+echo
+df -h
+echo
+free -m
+echo
+iostat
+echo
+echo ---End of script---
