55bc66b3b44529597ed1ae5f0920d7e711ded4db
arun <arunpatcha@>
2020-07-31 23:17:06 +0530

    Program 4 : variables.sh

---
+++ scripts/variable.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Purpose:  Define Variables
+# Author : Arun KC
+# Date : Fri Jul 31 23:10:23 IST 2020
+# modification: Fri Jul 31 23:10:23 IST 2020
+# Program 4
+
+
+p=pwd
+l=false
+wi=whoami
+d=date
+c='cal 2020'
+
+
+echo Run Variable Tasks
+echo
+$p
+echo
+$l
+echo
+$wi
+echo
+$d
+echo
+$c
+echo --end of script--
